0d76d87b40344c6650eaa3ef71c1d696952c2ef0
HannaPanda <sourcedog@mailbox.org>
2025-01-22 11:08:58 +0000

    Refactor PronomenCommand imports and clean up code.
    Updated `UserModel` import to default syntax for consistency. Removed unused `emitter` import and fixed formatting issues in template literals. Ensured cleaner and more maintainable code.

--- Commands/PronomenCommand.ts
+++ Commands/PronomenCommand.ts
@@ -1,8 +1,7 @@
-import emitter from "../emitter";
 import * as dotenv from "dotenv";
 import AbstractCommand from "../Abstracts/AbstractCommand";
 import sayService from "../Services/SayService";
-import { UserModel } from "../Models/User";
+import UserModel from "../Models/User";
 dotenv.config({ path: __dirname+'/../.env' });
 
 class PronomenCommand extends AbstractCommand {
@@ -13,8 +12,8 @@ class PronomenCommand extends AbstractCommand {
     command = 'pronomen';
     aliases = ['!pronouns'];
     description = 'Pronomen im Chat Hinweis';
-    answerNoTarget = `Eure Pronomen im Chat? Geht einfach auf https://pronouns.alejo.io/ und meldet euch über Twitch dort an. 
-        Dort könnt ihr eure Pronomen auswählen. 
+    answerNoTarget = `Eure Pronomen im Chat? Geht einfach auf https://pronouns.alejo.io/ und meldet euch über Twitch dort an.
+        Dort könnt ihr eure Pronomen auswählen.
         Dazu noch die Browserextension für Chrome: https://pronouns.alejo.io/chrome oder Firefox: https://pronouns.alejo.io/firefox emote_heart emote_heart emote_heart`;
     answerTarget = '';
     globalCooldown = 0;
